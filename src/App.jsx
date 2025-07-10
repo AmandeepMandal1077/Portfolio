bf87277eecba51c5af6f6b305f255e48abf52fe5
Amandeep <2023kucp1077@iiitkota.ac.in>
2025-07-10 18:01:45 +0530

    portfolio-structure

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Portfolio from "./components/Portfolio";
+
+function App() {
+  return (
+    <div className="w-full min-h-screen">
+      <Portfolio />
+    </div>
+  );
+}
+
+export default App;
